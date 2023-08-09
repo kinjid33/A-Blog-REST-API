9a84d280d7a6134f166b0fea413774f17f4b8954
Akinjide Akinyanmi <mrakinyanmi@gmail.com>
2023-08-09 14:56:29 +0100

    Created Module

---
+++ BAPI/src/main/java/com/madscicreative/bapi/BapiApplication.java
@@ -0,0 +1,13 @@
+package com.madscicreative.bapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BapiApplication.class, args);
+    }
+
+}
---
+++ BAPI/src/test/java/com/madscicreative/bapi/BapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madscicreative.bapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
